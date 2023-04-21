1f664a7f1f257457ca81c289fc3b0f83b3c67b65
U-keun Song <sukeun319@gmail.com>
2023-04-21 20:52:05 +0900

    DataSource 구성하기

--- src/test/java/org/zerock/springex/sample/SampleTest.java
+++ src/test/java/org/zerock/springex/sample/SampleTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @Log4j2
 @ExtendWith(SpringExtension.class) // JUnit5 버전에서 'spring-test'를 이용하기 위한 설정(JUnit4에서는 @Runwith)
 @ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml") // 스프링의 설정 정보를 로딩해온다.(현재 XML로 설정되어 있음)
@@ -16,10 +19,22 @@ public class SampleTest {
     @Autowired // 의존성 주입 관련 어노테이션; 해당 타입의 빈(Bean)이 존재하면 여기에 주입하라는 의미
     private SampleService sampleService;
     // 필드 주입(Field Injection) : 멤버 변수에 직접 @Autowired를 선언하는 방식
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     public void testService1() {
         log.info(sampleService);
         Assertions.assertNotNull(sampleService);
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+        Assertions.assertNotNull(connection);
+
+        connection.close();
     }
 }

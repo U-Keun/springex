e2c90961fdbdeca3e2accedbdacd059f6a73967f
U-keun Song <sukeun319@gmail.com>
2023-04-25 16:18:26 +0900

    단순 파라미터의 자동 수집 : @RequestParam

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -3,6 +3,7 @@ package org.zerock.springex.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // 해당 클래스가 스프링 MVC에서 컨트롤러 역할을 한다! -> 스프링 빈(Bean)
 @Log4j2
@@ -16,6 +17,14 @@ public class SampleController {
     @GetMapping("/ex1")
     public void ex1(String name, int age) {
         log.info("ex1...............");
+        log.info("name: " + name);
+        log.info("age: " + age);
+    }
+
+    @GetMapping("ex2")
+    public void ex2(@RequestParam(name = "name", defaultValue = "AAA") String name,
+                    @RequestParam(name = "age", defaultValue = "20") int age) {
+        log.info("ex2...........");
         log.info("name: " + name);
         log.info("age: " + age);
     }

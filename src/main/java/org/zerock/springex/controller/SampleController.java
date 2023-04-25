708421980ee404d7531360904f7a623a85466d4c
U-keun Song <sukeun319@gmail.com>
2023-04-25 17:00:30 +0900

    단순 파라미터의 자동 수집 : Formatter를 이용한 파라미터의 커스텀 처리

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
 @Controller // 해당 클래스가 스프링 MVC에서 컨트롤러 역할을 한다! -> 스프링 빈(Bean)
 @Log4j2
 public class SampleController {
@@ -27,5 +29,11 @@ public class SampleController {
         log.info("ex2...........");
         log.info("name: " + name);
         log.info("age: " + age);
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(LocalDate dueDate) {
+        log.info("ex3..........");
+        log.info("dueDate: " + dueDate);
     }
 }

a0c62518b827f151d5e22e2233f6f7b440e464a5
U-keun Song <sukeun319@gmail.com>
2023-04-25 17:29:08 +0900

    Model이라는 특별한 파라미터

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -2,8 +2,11 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.springex.dto.TodoDTO;
 
 import java.time.LocalDate;
 
@@ -35,5 +38,19 @@ public class SampleController {
     public void ex3(LocalDate dueDate) {
         log.info("ex3..........");
         log.info("dueDate: " + dueDate);
+    }
+
+    @GetMapping("/ex4")
+    public void ex4(Model model) {
+
+        log.info("---------------------");
+        model.addAttribute("message", "Hello World");
+    }
+
+    @GetMapping("/ex4_1")
+    public void ex4_1(@ModelAttribute("dto") TodoDTO todoDTO, Model model) {
+        // @ModelAttribute : JSP에서는 ${dto}와 같은 이름의 변수로 처리할 수 있다.
+        log.info(todoDTO);
+
     }
 }
---
+++ src/main/webapp/WEB-INF/views/ex4.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: u-keunsong
+  Date: 2023/04/25
+  Time: 5:17 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>${message}</h1>
+
+    <h1><c:out value="${message}"></c:out></h1>
+
+</body>
+</html>
